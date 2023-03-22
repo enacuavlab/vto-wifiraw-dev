3a9bb4fe329eaf7651ba5c0aac2ddb515644b985
pprz <xp31@free.fr>
2023-03-22 10:30:00 +0100

    up

--- test2/test_zfec_02.c
+++ test2/test_zfec_02.c
@@ -5,61 +5,58 @@
 #include <stdbool.h>
 #include "fec.h"
 
-
-/*****************************************************************************/
-int fec_k = 4;
-int fec_n = 8;
-
 #define PKT_DATA 1466
 
 /*****************************************************************************/
 int main(int argc, char *argv[]) {
 
-  uint8_t *enc_in[fec_n];
-  uint8_t enc_indata[fec_n][PKT_DATA];
-  for (int i=0;i<fec_n;i++) enc_in[i] = enc_indata[i];
+  uint8_t fec_k = 4, fec_n = 8; // fec_n = original data + extra data (fec_k)
 
-  uint8_t *enc_out[fec_k];
-  uint8_t enc_outdata[fec_k][PKT_DATA];
-  for (int i=0;i<fec_k;i++) enc_out[i] = enc_outdata[i];
+  uint8_t *enc_in[fec_k];        
+  uint8_t enc_indata[fec_k][PKT_DATA];
+  for (int i=0;i<fec_k;i++) enc_in[i] = enc_indata[i];
 
-  unsigned block_nums[] = { 0 + fec_k, 1 + fec_k, 2 + fec_k, 3 + fec_k };
+  uint8_t *enc_out[fec_n-fec_k];
+  uint8_t enc_outdata[fec_n][PKT_DATA];
+  for (int i=0;i<(fec_n-fec_k);i++) enc_out[i] = enc_outdata[i];
 
-  for (int i=0;i<fec_n;i++) {for (int j=0;j<PKT_DATA;j++) {enc_indata[i][j] = rand()%100;}}
+  unsigned block_nums[(fec_n-fec_k)]; // desired check blocks (id >= k), block_nums[i] >= code->k
+  for (int i=0;i<(fec_n-fec_k);i++) block_nums[i] = i + fec_k;
 
-  printf("original data [");for (int i=0;i < fec_n; i++) printf(" %d ",*enc_in[i]); printf(" ]\n");
+  for (int i=0;i<fec_k;i++) {for (int j=0;j<PKT_DATA;j++) {enc_indata[i][j] = rand()%100;}}
+
+  printf("original data [");for (int i=0;i < fec_k; i++) printf(" %d ",*enc_in[i]); printf(" ]\n");
   printf("encode inputs num [");for (int i=0;i < (fec_n-fec_k); i++) printf(" %d ",block_nums[i]); printf(" ]\n");
 
   fec_t *fec_p = fec_new(fec_k, fec_n);
   fec_encode(fec_p, (const uint8_t**)enc_in, enc_out, block_nums, (fec_n - fec_k), PKT_DATA);
   free(fec_p);
 
-  printf("encoded fec from data [");for (int i=0;i < fec_k; i++) printf(" %d ",*enc_out[i]); printf(" ]\n");
+  printf("encoded fec from data [");for (int i=0;i < (fec_n-fec_k); i++) printf(" %d ",*enc_out[i]); printf(" ]\n");
 
   printf("send and receive data and tagged fec frames\n");
 
-  uint8_t *data_frame[fec_n];
-  for (int i=0;i<fec_n;i++) data_frame[i] = enc_in[i];
+  uint8_t *data_frame[(fec_n-fec_k)];
+  for (int i=0;i<(fec_n-fec_k);i++) data_frame[i] = enc_in[i];   // get data
   uint8_t *fec_frame[fec_k];
-  for (int i=0;i<fec_k;i++) fec_frame[i] = enc_out[i];
+  for (int i=0;i<fec_k;i++) fec_frame[i] = enc_out[i];          // get encoded data
 
-  printf("data frame is alterated\n");
+  printf("set recovery options\n");
 
   unsigned indexes[fec_k];
-
+  uint8_t *dec_in[fec_k];
   bool map[fec_n];
-  memset(map,0,sizeof(map));
+  memset(map,0,sizeof(map)); // set with no failures
 
-  // Recovery for first 0 to fec_k, position in data frames
-  // Data frames from fec_k to fec_n, cannot be recover (due to choosen fec encode index)
+  // Recovery for 0 to fec_k, position in data frames
 
   // Uncomment one of these lines to make the test
 
 // single failure
 // map // not set
-//  map[0] = 1; 
+//  map[0] = 1;
 //  map[1] = 1;
-//  map[2] = 1; 
+//  map[2] = 1;
 //  map[3] = 1;
 
   // multiple failure
@@ -72,10 +69,6 @@ int main(int argc, char *argv[]) {
 //  map[0] = 1; map[1] = 1; map[2] = 1;
 //  map[1] = 1; map[2] = 1; map[3] = 1;
 //  map[0] = 1; map[1] = 1; map[3] = 1;
-
-  uint8_t *dec_in[fec_k];
-  uint8_t *dec_out[fec_n - fec_k];
-  uint8_t dec_outdata[fec_n - fec_k][PKT_DATA];
 
   for(int i=0; i < fec_k; i++)   {
     if(map[i]) {
@@ -90,7 +83,9 @@ int main(int argc, char *argv[]) {
   printf("decode inputs input [");for (int i=0;i < fec_k; i++) printf(" %d ",*dec_in[i]); printf(" ]\n");
   printf("decode inputs index [");for (int i=0;i < fec_k; i++) printf(" %d ",indexes[i]); printf(" ]\n");
 
-  for (int i=0;i<(fec_n - fec_k);i++) {dec_out[i] = dec_outdata[i];memset(dec_out[i],0,PKT_DATA);}
+  uint8_t *dec_out[(fec_n-fec_k)]; // only packet not present in input, will be reconstruct and writtent
+  uint8_t dec_outdata[(fec_n-fec_k)][PKT_DATA];
+  for (int i=0;i<(fec_n-fec_k);i++) {dec_out[i] = dec_outdata[i];memset(dec_out[i],0,PKT_DATA);}
 
   fec_p = fec_new(fec_k, fec_n);
   fec_decode(fec_p, (const uint8_t**)dec_in, dec_out, indexes, PKT_DATA);
@@ -98,16 +93,14 @@ int main(int argc, char *argv[]) {
 
   printf("decode outputs [");for (int i=0;i < (fec_n-fec_k); i++) printf(" %d ",*dec_out[i]); printf(" ]\n");
 
-  uint8_t *data_frame_out[fec_n];
-  for (int i=fec_k;i<fec_n;i++) data_frame_out[i] = data_frame[i]; 
-
+  uint8_t *data_frame_out[fec_k];
   uint8_t error_pos = 0;
   for (int i=0;i<fec_k;i++) {
     if (map[i]) {
-      data_frame_out[i] = dec_out[error_pos]; 
+      data_frame_out[i] = dec_out[error_pos];
       error_pos++;
     } else { data_frame_out[i] = dec_in[i];}
   }
 
-  printf("rebuild outputs [");for (int i=0;i < fec_n; i++) printf(" %d ",*data_frame_out[i]); printf(" ]\n");
+  printf("rebuild outputs [");for (int i=0;i < fec_k; i++) printf(" %d ",*data_frame_out[i]); printf(" ]\n");
 }
