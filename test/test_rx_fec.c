7a45e7e5095cbe9af4d2021e732ea368349bbbee
pprz <xp31@free.fr>
2023-04-02 20:20:20 +0200

    up

--- test/test_rx_fec.c
+++ test/test_rx_fec.c
@@ -8,6 +8,13 @@ typedef struct {
 } wifi_adapter_rx_status_t;
 wifi_adapter_rx_status_t rx_status;
 
+
+typedef struct {
+  bool crc;
+  uint32_t len;
+  uint8_t buf[PKT_SIZE];
+} pkt_t;
+
 /*****************************************************************************/
 #define RADIOTAP_DBM_ANTSIGNAL_OFF 22
 
@@ -61,8 +68,16 @@ int main(int argc, char *argv[]) {
   int fd = pcap_get_selectable_fd(ppcap);
 
   struct pcap_pkthdr *hdr = NULL;
-  uint8_t pkt[PKT_SIZE], cpt_d=0;
+  pkt_t pkt[fec_n];
+  uint8_t cpt_n=0;
+  uint8_t *pu8;
+
+  uint32_t crc, bytes, dataLen, captlimit, payloadSize, inl;
+  uint16_t u16HeaderLen;
   
+  uint8_t num_fec[fec_k], cpt_k=0;
+  uint8_t num_data[fec_d], cpt_d=0;
+
   for(;;) {
     fd_set readset;
     FD_ZERO(&readset);
@@ -70,94 +85,30 @@ int main(int argc, char *argv[]) {
     int n = select(fd+1, &readset, NULL, NULL, NULL);
     if(n == 0) break;
     if(FD_ISSET(fd, &readset)) {  // Less CPU consumption than pcap_loop()
-  
-      if (1 == pcap_next_ex(ppcap, &hdr, (const u_char**)&(pkt))) {
  
-        uint32_t crc;
-        uint32_t bytes = (hdr->len);
-        uint16_t u16HeaderLen = (pkt[2] + (pkt[3] << 8)); // variable radiotap header size
-        uint32_t dataLen = bytes - u16HeaderLen - sizeof(crc);
-        uint32_t captlimit = u16HeaderLen + sizeof(wifi_hdr) + sizeof(llc_hdr) + sizeof(uint32_t); // 4 bytes CRC32
+      pu8 = (pkt[cpt_n].buf);
+
+      if (1 == pcap_next_ex(ppcap, &hdr, (const u_char**)&pu8)) {
+ 
+        bytes = (hdr->len);
+        u16HeaderLen = (pu8[2] + (pu8[3] << 8)); // variable radiotap header size
+        dataLen = bytes - u16HeaderLen - sizeof(crc);
+        captlimit = u16HeaderLen + sizeof(wifi_hdr) + sizeof(llc_hdr) + sizeof(uint32_t); // 4 bytes CRC32
   
         rx_status.rcv_pkt_cnt ++;
         if (bytes >= captlimit) {
-          rx_status.signal_dbm = pkt[RADIOTAP_DBM_ANTSIGNAL_OFF];
-  
-          const uint8_t *s = &pkt[u16HeaderLen]; // compute CRC32 for [sizeof(wifi_hdr) + sizeof(llc_hdr) + data]
-          uint32_t crc=0xFFFFFFFF;
-          for(uint32_t i=0;i<dataLen;i++) {
-            uint8_t ch=s[i];
-            uint32_t t=(ch^crc)&0xFF;
-            crc=(crc>>8)^crc32_table[t];
-          }
-  
-          uint32_t crc_rx;                 // retrieve CRC32 from last uint32_t
-          memcpy(&crc_rx, &pkt[bytes - sizeof(crc_rx)], sizeof(crc_rx));
-  
-          if (crc_rx!=~crc)rx_status.wrong_crc_cnt++;
-          else {
-            uint32_t payloadSize = bytes - captlimit;
-            const uint8_t *pu8 = &pkt[captlimit - sizeof(uint32_t)];
-            if (payloadSize > 0) {
-              uint32_t inl;
-              memcpy(&inl,pu8, sizeof(inl));
-              pu8 += sizeof(inl);
-              if ((int16_t)inl > 0) { // This is for data frames
-//                write(STDOUT_FILENO, pu8, inl);
-                printf("write inl=(%d)\n",inl);            
-  	      } else printf("Negative (%d)\n",inl);
-            }
-          }
-        }
-      }
-    }
-  }
-}
 
-/*
-  pkt_t pkt[fec_n]; uint8_t cpt_n=0;
-  uint8_t num_fec[fec_k], cpt_fec=0;
-  uint8_t num_data[fec_d], cpt_data=0;
-
-  for(;;) {
-    fd_set readset;
-    FD_ZERO(&readset);
-    FD_SET(fd, &readset);
-    int n = select(fd+1, &readset, NULL, NULL, NULL);
-    if(n == 0) break;
-    if(FD_ISSET(fd, &readset)) {  // Less CPU consumption than pcap_loop()
-
-      struct pcap_pkthdr *hdr = NULL;
-
-      uint8_t payloadBuffer[PKT_SIZE];
-      uint8_t *pkt = payloadBuffer;
-
-//      uint8_t *pu8 = (pkt[cpt_n].buf);
-
-      if (1 == pcap_next_ex(ppcap, &hdr, (const u_char**)&pkt)) {
-
-        uint32_t crc;
-        uint32_t bytes = (hdr->len);
-        uint16_t u16HeaderLen = (pkt[2] + (pkt[3] << 8)); // variable radiotap header size
-        uint32_t dataLen = bytes - u16HeaderLen - sizeof(crc);
-        uint32_t captlimit = u16HeaderLen + sizeof(wifi_hdr) + sizeof(llc_hdr) + sizeof(uint32_t); // 4 bytes CRC32
-      
-        rx_status.rcv_pkt_cnt ++;  
-      
-        if (bytes >= captlimit) {
-          uint32_t payloadSize = bytes - captlimit;
+          payloadSize = bytes - captlimit;
           if (payloadSize > 0) {
+          
+            pu8 = pu8 + captlimit - sizeof(uint32_t);
+            memcpy(&inl,pu8, sizeof(inl));
+            pu8 += sizeof(inl);
 
-//            const uint8_t *pu8_pay = &pu8[captlimit - sizeof(uint32_t)];
-            const uint8_t *pu8 = &pkt[captlimit - sizeof(uint32_t)];
+            if ((int16_t)inl > 0) { // This is for data frames
 
-            uint16_t len;
-            memcpy(&len, pu8, sizeof(len));
-            pu8 += sizeof(len);
-
-	    if ((int16_t)len > 0) { // This is for data frames
-
-              const uint8_t *s = &pkt[u16HeaderLen]; // compute CRC32 for [sizeof(wifi_hdr) + sizeof(llc_hdr) + data]
+	      num_data[cpt_d++] = cpt_n;
+              const uint8_t *s = pu8 + u16HeaderLen; // compute CRC32 for [sizeof(wifi_hdr) + sizeof(llc_hdr) + data]
               uint32_t crc=0xFFFFFFFF;
               for(uint32_t i=0;i<dataLen;i++) {
                 uint8_t ch=s[i];
@@ -165,42 +116,26 @@ int main(int argc, char *argv[]) {
                 crc=(crc>>8)^crc32_table[t];
               }
               uint32_t crc_rx;                 // retrieve CRC32 from last uint32_t
-              memcpy(&crc_rx, &pkt[bytes - sizeof(crc_rx)], sizeof(crc_rx));
-//              pkt[cpt_n].crc = (crc_rx==~crc);
-//              pkt[cpt_n].len = len;
-//	      num_data[cpt_data++] = cpt_n;
-              rx_status.signal_dbm = pkt[RADIOTAP_DBM_ANTSIGNAL_OFF];
+              memcpy(&crc_rx, &pu8[bytes - sizeof(crc_rx)], sizeof(crc_rx));
+	      pkt[cpt_n].crc = (crc_rx!=~crc);
 
-              write(STDOUT_FILENO, pu8, len);
-              fflush(stdout);
+              if (crc_rx!=~crc)rx_status.wrong_crc_cnt++;
+              rx_status.signal_dbm = pu8[RADIOTAP_DBM_ANTSIGNAL_OFF];
+
+//                write(STDOUT_FILENO, pu8, inl);
+ //             printf("write inl=(%d)\n",inl);            
+  	    } else {
+//	      printf("Negative (%d)\n",inl);
+              num_fec[cpt_k++] = cpt_n;
 	    }
 	  }
-	}
-      }
-    }
-  }
-}
-	    } else {              // This is for fec frames
-
-              pkt[cpt_n].len = -len;
-	      num_fec[cpt_fec++] = cpt_n;
-
-	    }
-
-	    if ((cpt_data==fec_d)&&(cpt_fec==fec_k)) {
-	      for (int i=0; i<fec_d;i++) {
-		pu8 = (pkt[num_data[i]].buf);
-                pu8_pay = &pu8[captlimit];
-                write(STDOUT_FILENO, pu8_pay, pkt[num_data[i]].len);
-                fflush(stdout);
-	      }
-	      cpt_n=-1;cpt_data=0;cpt_fec=0;
-	    }
-          }
         }
+	printf("(%d)(%d)(%d)\n",cpt_n,cpt_k,cpt_d);
 	cpt_n++;
+	if (cpt_d == fec_d) cpt_d=0;
+	if (cpt_k == fec_k) cpt_k=0;
+	if (cpt_n == fec_n) cpt_n=0;
       }
     }
   }
 }
-*/
