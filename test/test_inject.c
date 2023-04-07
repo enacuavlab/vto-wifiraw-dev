5c4d1934e9b7a857736096c44cf6046346731cc6
amtcvx <amtcvx@gmail.com>
2023-04-07 16:59:03 +0200

    up

--- test/test_inject.c
+++ test/test_inject.c
@@ -6,17 +6,59 @@
 #include <unistd.h>
 
 static const uint8_t radiotap_hdr[] = {
-  0x00, 0x00, // <-- radiotap version
-  0x00, 0x00, // <- radiotap header length
-  0x6f, 0x08, 0x00, 0x00, // <-- bitmap
-  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // <-- timestamp
-  0x00, // <-- flags
-  0x48, // <-- rate 36 Mb
-  0x71, 0x09, 0xc0, 0x00, // <-- channel
-  0xde, // <-- antsignal
-  0x00, // <-- antnoise
-  0x01, // <-- antenna
+	0x00, 0x00, // <-- radiotap version + pad byte
+	0x0b, 0x00, // <- radiotap header length
+	0x04, 0x0c, 0x00, 0x00, // <-- bitmap
+	0x60, // <-- rate (in 500kHz units)
+	0x0c, //<-- tx power
+	0x01 //<-- antenna
 };
+
+/*
+#define IEEE80211_RADIOTAP_MCS_HAVE_BW    0x01
+#define IEEE80211_RADIOTAP_MCS_HAVE_MCS   0x02
+#define IEEE80211_RADIOTAP_MCS_HAVE_GI    0x04
+#define IEEE80211_RADIOTAP_MCS_HAVE_FMT   0x08
+
+#define IEEE80211_RADIOTAP_MCS_BW_20    0
+#define IEEE80211_RADIOTAP_MCS_BW_40    1
+#define IEEE80211_RADIOTAP_MCS_BW_20L   2
+#define IEEE80211_RADIOTAP_MCS_BW_20U   3
+#define IEEE80211_RADIOTAP_MCS_SGI      0x04
+#define IEEE80211_RADIOTAP_MCS_FMT_GF   0x08
+#define IEEE80211_RADIOTAP_MCS_HAVE_FEC   0x10
+#define IEEE80211_RADIOTAP_MCS_HAVE_STBC  0x20
+
+#define IEEE80211_RADIOTAP_MCS_FEC_LDPC   0x10
+#define IEEE80211_RADIOTAP_MCS_STBC_MASK  0x60
+#define IEEE80211_RADIOTAP_MCS_STBC_1  1
+#define IEEE80211_RADIOTAP_MCS_STBC_2  2
+#define IEEE80211_RADIOTAP_MCS_STBC_3  3
+#define IEEE80211_RADIOTAP_MCS_STBC_SHIFT 5
+
+#define MCS_KNOWN (IEEE80211_RADIOTAP_MCS_HAVE_MCS | IEEE80211_RADIOTAP_MCS_HAVE_BW | IEEE80211_RADIOTAP_MCS_HAVE_GI | IEEE80211_RADIOTAP_MCS_HAVE_STBC | IEEE80211_RADIOTAP_MCS_HAVE_FEC)
+
+// For MCS#1 -- QPSK 1/2 20MHz long GI + STBC
+//#define MCS_FLAGS  (IEEE80211_RADIOTAP_MCS_BW_20 | (IEEE80211_RADIOTAP_MCS_STBC_1 << IEEE80211_RADIOTAP_MCS_STBC_SHIFT))
+
+// for MCS#1 -- QPSK 1/2 20MHz long GI without STBC
+//#define MCS_FLAGS  (IEEE80211_RADIOTAP_MCS_BW_20)
+
+// for stbc + ldpc: https://en.wikipedia.org/wiki/Space%E2%80%93time_block_code
+#define MCS_FLAGS  (IEEE80211_RADIOTAP_MCS_BW_20 | (IEEE80211_RADIOTAP_MCS_STBC_1 << IEEE80211_RADIOTAP_MCS_STBC_SHIFT) | IEEE80211_RADIOTAP_MCS_FEC_LDPC)
+
+#define MCS_INDEX  1
+
+static uint8_t radiotap_hdr[]  __attribute__((unused)) = {
+    0x00, 0x00, // <-- radiotap version
+    0x0d, 0x00, // <- radiotap header length
+    0x00, 0x80, 0x08, 0x00, // <-- radiotap present flags:  RADIOTAP_TX_FLAGS + RADIOTAP_MCS
+    0x08, 0x00,  // RADIOTAP_F_TX_NOACK
+    MCS_KNOWN , MCS_FLAGS, MCS_INDEX // bitmap, flags, mcs_index
+};
+
+
+*/
 
 static const char wifi_hdr[] = {
   0x88, 0x00, 0x30, 0x00,             // frame type to match on receiver
