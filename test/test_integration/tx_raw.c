3f58f31afabeebc7a20e5af41f7b557b2887cf39
pprz <pprz@choubaca>
2023-04-23 14:38:40 +0200

    up

--- test/test_integration/tx_raw.c
+++ test/test_integration/tx_raw.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[]) {
     memset(buf_d[i], 0, sizeof (PKT_SIZE));
     memcpy(buf_d[i], uint8_taRadiotapHeader, sizeof (uint8_taRadiotapHeader));
     memcpy(&buf_d[i][sizeof(uint8_taRadiotapHeader)], &ieee_hdr_data, sizeof(ieee_hdr_data));
-    buf_d[i][9] = param_portid;
+    buf_d[i][44] = param_portid;
   }
 
   uint16_t fd_in = 0;
@@ -34,8 +34,7 @@ int main(int argc, char *argv[]) {
   addr.sin_family = AF_INET;
   addr.sin_port = htons(5000);
   addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  uint16_t r;
-  if((r = bind(fd_in, (struct sockaddr *)&addr, sizeof(addr))) == -1) exit(-1);
+  if (-1 == bind(fd_in, (struct sockaddr *)&addr, sizeof(addr))) exit(-1);
 
   uint16_t fd_out = 0;
   if (-1 == (fd_out=socket(AF_PACKET,SOCK_RAW,IPPROTO_RAW))) exit(-1);
@@ -48,16 +47,15 @@ int main(int argc, char *argv[]) {
   sll.sll_family   = AF_PACKET;
   sll.sll_ifindex  = ifr.ifr_ifindex;
   sll.sll_protocol = htons( ETH_P_ALL );
-  if((r = bind(fd_out, (struct sockaddr *)&sll, sizeof(sll))) == -1) exit(-1);
+  if (-1 == bind(fd_out, (struct sockaddr *)&sll, sizeof(sll))) exit(-1);
 
   struct timeval timeout;
   struct timespec stp;
   uint64_t stp_n, delay_n=0;
   uint32_t inl, data_size = DATA_SIZE;
-  uint16_t offset,len,seq=0;
+  uint16_t offset,len,seq=0i,wait_u,delta_u,r;
   uint8_t di;
   uint8_t *pu8;
-  int32_t delta1_u,delta2_u;
   for(;;) {
     fd_set readset;
     FD_ZERO(&readset);
@@ -67,7 +65,6 @@ int main(int argc, char *argv[]) {
     if (r > 0) {     
       if (len_d[cpt_d] == 0) offset = headerSize1;
       inl = read(fd_in, &(buf_d[cpt_d][offset]), data_size - len_d[cpt_d] );   // fill pkts with read input
-      printf("(%d)(%d)\n",cpt_d,inl);fflush(stdout);									       
       if (inl < 0) continue;
       len_d[cpt_d] += inl;
       offset += inl;
@@ -80,31 +77,19 @@ int main(int argc, char *argv[]) {
         while (di < fec_d) {
 	  if (len_d[di] == 0) di = fec_d;
 	  else {
-
             pu8 = buf_d[di]; len = len_d[di] ; len_d[di] = 0; di ++;
-  
             clock_gettime( CLOCK_MONOTONIC, &stp);
             stp_n = (stp.tv_nsec + (stp.tv_sec * 1000000000L));
-  
             (((pay_hdr_t *)(&(pu8[headerSize0])))->seq) = seq;
             (((pay_hdr_t *)(&(pu8[headerSize0])))->len) = len;
             (((pay_hdr_t *)(&(pu8[headerSize0])))->stp_n) = stp_n;
-  
-//            r = write(fd_out, pu8, PKT_SIZE);
-//            if (r != PKT_SIZE) exit(-1);
-  
-	    if (delay_n == 0) {
-              delta1_u = 0;
-	    } else {
-	      delta1_u = (400 - delta2_u - ((stp_n - delay_n)/1000)) ;
-	    }
+            r = write(fd_out, pu8, PKT_SIZE);
+            if (r != PKT_SIZE) exit(-1);
+	    delta_u = (stp_n - delay_n)/1000;
+	    if (delta_u > 400) wait_u = 0;
+	    else wait_u = 400 - delta_u;
 	    delay_n = stp_n;
-            delta2_u = delta1_u;
-
-	    printf("-->(%d)\n", 400 - delta1_u);fflush(stdout);
-//	    if (delta_u < 0) exit(-1);
-            usleep( 400 - delta1_u );
-
+            usleep( wait_u );
 	  }
 	}
 	cpt_d = 0; di = 0;
