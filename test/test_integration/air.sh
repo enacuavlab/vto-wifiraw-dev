64e6ada6c3a7431746de60cf441573d853883590
amtcvx <xp31@free.fr>
2023-05-24 21:43:39 +0200

    Update air.sh

--- test/test_integration/air.sh
+++ test/test_integration/air.sh
@@ -10,3 +10,5 @@ sleep 1
 ifconfig airtuntx mtu 1400 up &
 while [ ! "`sysctl -w net.ipv4.conf.airtunrx.rp_filter=2`" = "net.ipv4.conf.airtunrx.rp_filter = 2" ];do sleep 1; done
 route add default airtuntx  > /dev/null 2>&1 &
+ 
+gst-launch-1.0 libcamerasrc! video/x-raw,width=1280,height=720,framerate=25/1 ! timeoverlay ! x264enc tune=zerolatency bitrate=5000 ! rtph264pay mtu=1400 config-interval=-1 ! udpsink port=5600 host=127.0.0.1 
