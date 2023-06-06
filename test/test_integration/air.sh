afe04861941c78635f5733ab2ed4cbecc6185e93
amtcvx <amtcvx@gmail.com>
2023-06-06 15:10:11 +0200

    up

--- test/test_integration/air.sh
+++ test/test_integration/air.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-export node wlx3c7c3fa9c1e8
+#export node wlx3c7c3fa9c1e8
 
-$PWD/wifiraw 1 $node &
+#$PWD/wifiraw 1 $node &
 
-socat TUN:10.0.1.2/24,tun-name=airtuntx,iff-no-pi,tun-type=tun,iff-up udp-sendto:127.0.0.1:14900 > /dev/null 2>&1 &
-socat udp-listen:14800,reuseaddr,fork TUN:10.0.1.2/24,tun-name=airtunrx,iff-no-pi,tun-type=tun,iff-up > /dev/null 2>&1 &
-sleep 1
-ifconfig airtuntx mtu 1400 up &
-while [ ! "`sysctl -w net.ipv4.conf.airtunrx.rp_filter=2`" = "net.ipv4.conf.airtunrx.rp_filter = 2" ];do sleep 1; done
-route add default airtuntx  > /dev/null 2>&1 &
+#socat TUN:10.0.1.2/24,tun-name=airtuntx,iff-no-pi,tun-type=tun,iff-up udp-sendto:127.0.0.1:14900 > /dev/null 2>&1 &
+#socat udp-listen:14800,reuseaddr,fork TUN:10.0.1.2/24,tun-name=airtunrx,iff-no-pi,tun-type=tun,iff-up > /dev/null 2>&1 &
+#sleep 1
+#ifconfig airtuntx mtu 1400 up &
+#while [ ! "`sysctl -w net.ipv4.conf.airtunrx.rp_filter=2`" = "net.ipv4.conf.airtunrx.rp_filter = 2" ];do sleep 1; done
+#route add default airtuntx  > /dev/null 2>&1 &
 
-gst-launch-1.0 videotestsrc ! video/x-raw,width=1280,height=720,framerate=25/1 ! timeoverlay ! x264enc tune=zerolatency bitrate=5000 ! rtph264pay mtu=1400 config-interval=-1 ! udpsink port=5600 host=127.0.0.1
+gst-launch-1.0     videotestsrc ! video/x-raw,width=1280,height=720,framerate=30/1,format=NV12,interlace-mode=progressive,colorimetry=bt709 !     v4l2h264enc extra-controls="controls,video_bitrate_mode=0,h264_minimum_qp_value=35,h264_maximum_qp_value=35,h264_i_frame_period=30,h264_profile=0,h264_level=11,video_bitrate=2000;" ! video/x-h264,level="(string)4" ! rtph264pay mtu=1400 config-interval=-1 ! udpsink port=5600 host=127.0.0.1
 
-gst-launch-1.0 libcamerasrc! video/x-raw,width=1280,height=720,framerate=25/1 ! timeoverlay ! x264enc tune=zerolatency bitrate=5000 ! rtph264pay mtu=1400 config-interval=-1 ! udpsink port=5600 host=127.0.0.1 
+gst-launch-1.0     libcamerasrc ! video/x-raw,width=1280,height=720,framerate=30/1,format=NV12,interlace-mode=progressive,colorimetry=bt709 !     v4l2h264enc extra-controls="controls,video_bitrate_mode=0,h264_minimum_qp_value=35,h264_maximum_qp_value=35,h264_i_frame_period=30,h264_profile=0,h264_level=11,video_bitrate=2000;" ! video/x-h264,level="(string)4" ! rtph264pay mtu=1400 config-interval=-1 ! udpsink port=5600 host=127.0.0.1
+
+
--- test/test_integration/ground.sh
+++ test/test_integration/ground.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-export node wlx3c7c3fa9c1e8
+#export node wlx3c7c3fa9c1e8
 
-$PWD/wifiraw 0 $node &
+#$PWD/wifiraw 0 $node &
 
-socat TUN:10.0.1.1/24,tun-name=groundtuntx,iff-no-pi,tun-type=tun,iff-up udp-sendto:127.0.0.1:14800 > /dev/null 2>&1 &
-socat udp-listen:14900,reuseaddr,fork  TUN:10.0.1.1/24,tun-name=groundtunrx,iff-no-pi,tun-type=tun,iff-up > /dev/null 2>&1 &
-sleep 1
-ifconfig groundtuntx mtu 1400 up &
-while [ ! "`sysctl -w net.ipv4.conf.groundtunrx.rp_filter=2`" = "net.ipv4.conf.groundtunrx.rp_filter = 2" ];do sleep 1; done
+#socat TUN:10.0.1.1/24,tun-name=groundtuntx,iff-no-pi,tun-type=tun,iff-up udp-sendto:127.0.0.1:14800 > /dev/null 2>&1 &
+#socat udp-listen:14900,reuseaddr,fork  TUN:10.0.1.1/24,tun-name=groundtunrx,iff-no-pi,tun-type=tun,iff-up > /dev/null 2>&1 &
+#sleep 1
+#ifconfig groundtuntx mtu 1400 up &
+#while [ ! "`sysctl -w net.ipv4.conf.groundtunrx.rp_filter=2`" = "net.ipv4.conf.groundtunrx.rp_filter = 2" ];do sleep 1; done
 
 gst-launch-1.0 udpsrc port=5600 ! application/x-rtp, encoding-name=H264, payload=96 ! rtph264depay ! h264parse ! queue ! avdec_h264 !  videoconvert ! autovideosink sync=false
