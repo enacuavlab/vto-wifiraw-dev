c78741aa949dae1d37933e5c59c177ab0bd1de8f
amtcvx <amtcvx@gmail.com>
2023-06-09 13:08:19 +0200

    up

--- src/setmon.sh
+++ src/setmon.sh
@@ -1,11 +1,12 @@
-#declare -a usbwifidongles=(wlx3c7c3fa9c1e4 wlxfc34972ed57c)
-declare -a usbwifidongles=(wlxfc349725a319 wlxfc349725a317)
+declare -a usbwifidongles=(wlan1)
+#declare -a usbwifidongles=(wlx7c10c91c408e wlx3c7c3fa9c1e8)
+#declare -a usbwifidongles=(wlxfc349725a319 wlxfc349725a317)
 
 for dongle in "${usbwifidongles[@]}"
 do 
   echo "$dongle"
-  sudo ifconfig $dongle down
-  sudo ifconfig $dongle up
-  sudo iwconfig $dongle mode monitor
-  sudo iwconfig $dongle channel 36
+  sudo ip link set $dongle down
+  sudo iw dev $dongle set type monitor
+  sudo ip link set $dongle up
+  sudo iw dev $dongle set channel 140
 done
