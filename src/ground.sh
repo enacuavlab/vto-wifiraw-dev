4549fe8a1351c94cb7dcaad301e1ff1b96b1ecb4
amtcvx <amtcvx@gmail.com>
2023-06-22 16:58:51 +0200

    up

--- src/ground.sh
+++ src/ground.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+socat -u udp-listen:4244,reuseaddr,fork -
 
-#export node wlx3c7c3fa9c1e8
+/home/pprz/Projects/paparazzi/sw/ground_segment/tmtc/server  -no_md5_check option
+/home/pprz/Projects/paparazzi/sw/tools/gcs_launch.py
+/home/pprz/Projects/paparazzi/sw/ground_segment/tmtc/link_py.py -d /dev/ttyUSB0 -t xbee -s 57600 -ac 114:127.0.0.1:4244:4245
 
-#$PWD/wifiraw 0 $node &
-
-#gst-launch-1.0 udpsrc port=5600 ! application/x-rtp, encoding-name=H264, payload=96 ! rtph264depay ! h264parse ! queue ! avdec_h264 !  videoconvert ! autovideosink sync=false
+gst-launch-1.0 udpsrc port=5600 ! application/x-rtp, encoding-name=H264, payload=96 ! rtph264depay ! h264parse ! queue ! avdec_h264 !  videoconvert ! autovideosink sync=false
