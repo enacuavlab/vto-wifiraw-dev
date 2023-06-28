d1dcc99a329dffdfb00b94854e353f8472d4c338
amtcvx <xp31@free.fr>
2023-06-28 08:00:48 +0200

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -17,7 +17,7 @@ if uname -a | grep -cs "Ubuntu"> /dev/null 2>&1;then DKMS=true; fi
 if uname -a | grep -cs "4.9.253-tegra"> /dev/null 2>&1;then DKMS=true; fi
 if $DKMS; then
   sudo sed -i  '/blacklist rtl8812au/d' /etc/modprobe.d/blacklist.conf > /dev/null 2>&1
-  drivername=`dkms status | grep 8812 | awk '{print substr($1,1,length($1)-1)"/"substr($2,1,length($2)-1)'` 
+  drivername=`dkms status | grep 8812 | awk '{print substr($1,1,length($1)-1)}'` 
   echo $drivername
   sudo dkms uninstall $drivername
   sudo dkms remove $drivername --all
