4b611045408ff432f428b878dd11f4945172cc4c
amtcvx <xp31@free.fr>
2023-07-23 15:15:21 +0200

    Update proxy.sh

--- src/proxy.sh
+++ src/proxy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#socat udp-listen:5600,reuseaddr,fork udp-sendto:192.168.2.1:5600
+#socat -b1400 udp-listen:5600,reuseaddr,fork udp-sendto:192.168.2.1:5600
 socat udp-listen:4244,reuseaddr,fork udp-sendto:192.168.2.1:4244
 
